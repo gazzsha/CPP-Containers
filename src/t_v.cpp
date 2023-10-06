a385a75a66e86fd65e72ff246cab42894d893364
antoinco <antoinco@student.21-school.ru>
2023-10-06 19:02:24 +0300

    fix vect

--- src/t_v.cpp
+++ src/t_v.cpp
@@ -196,3 +196,32 @@
 //   s21::vector<int> s21_v2{4, 5, 6};
 //   s21_v2 = std::move(s21_v1);
 // }
+
+#include <iostream>
+#include <vector>
+
+#include "s21_vector.h"
+struct Base {
+  Base() { print(); }
+  virtual void print() { std::cout << "Base\n"; }
+  ~Base() { print(); }
+};
+
+struct Derived : Base {
+  Derived() { print(); }
+  void print() override { std::cout << "Derived\n"; }
+  ~Derived() { print(); }
+};
+
+int main() {
+  Base* base = new Derived();
+  base->print();
+
+  delete base;
+
+  s21::vector<int> v{1, 23, 4};
+  v.swap(v);
+  for (auto n : v) {
+    std::cout << n << " ";
+  }
+}
