1f2621708868c02ebd96625d6e324abab8aa3d9f
antoinco <antoinco@student.21-school.ru>
2023-10-10 21:15:27 +0300

    some headers

--- src/Testing/unit_test.h
+++ src/Testing/unit_test.h
@@ -1,3 +1,6 @@
+
+#ifndef CPP2_S21_CONTAINERS_SRC_TESTING_UNIT_TEST_H_  //
+#define CPP2_S21_CONTAINERS_SRC_TESTING_UNIT_TEST_H_
 #include <gtest/gtest.h>
 
 #include <array>
@@ -9,3 +12,5 @@
 #include <vector>
 
 #include "../s21_containers.h"
+#include "../s21_containersplus.h"
+#endif  // CPP2_S21_CONTAINERS_SRC_TESTING_UNIT_TEST_H_
